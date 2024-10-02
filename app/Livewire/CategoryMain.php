29f98fe5a2f4e81689728cc681dfe9b8c929b1a5
williamOrtiz <136346592+302xdwill@users.noreply.github.com>
2024-10-01 19:15:12 -0500

    CategoryMain.php

--- app/Livewire/CategoryMain.php
+++ app/Livewire/CategoryMain.php
@@ -7,43 +7,49 @@ use App\Models\Category;
 use Livewire\Attributes\On;
 use Livewire\Component;
 
-class CategoryMain extends Component{
+class CategoryMain extends Component
+{
     public CategoryForm $form;
     public $isOpen=false;
     public $search;
 
-    public function render(){
-        $categories=Category::where('name','LIKE','%'.$this->search.'%')->latest('id')->paginate();
+    public function render()
+    {
+        $categories=Category::where('name','LIKE','%'. $this->search . '%')->paginate();
         return view('livewire.category-main',compact('categories'));
     }
 
-    public function create(){
-        $this->isOpen=false;
-        $this->form->reset();
-        $this->resetValidation();
-    }
-
-    public function store(){
-        //dd($this->form->all());
-        $this->validate();
-        if($this->form->id==null){
-            Category::create($this->form->all());
-        }else{
-            $category=Category::find($this->form->id);
-            $category->update($this->form->all());
-        }
-        $this->isOpen=false;
-        $this->dispatch('sweetalert',message:'Registro creado');
-    }
-
-    public function edit(Category $category){
-        $this->isOpen=true;
-        $this->form->fill($category);
-    }
-
     #[On('delItem')]
-    public function destroy(Category $category){
+    public function destroy(  Category $category){
         //dd($post);
         $category->delete();
+    }
+
+    public function create(){
+        $this->isOpen=true;
+        $this->form->reset();
+        $this->resetValidation();
+
+    }
+
+    public function store() {
+        //dd($this->form->all());
+        $this->validate();
+        $this->form->name = str_replace(" ", "-", $this->form->name);
+
+        if($this->form->id == null) {
+            Category::create($this->form->all());
+        } else {
+            $category = Category::find($this->form->id);
+            $category->update($this->form->all());
+        }
+
+        $this->isOpen = false;
+        $this->dispatch('sweetalert', ['message' => 'Registro creado']);
+    }
+
+    public function edit(Category $category) {
+        $this->isOpen = true;
+        $this->form->fill($category);
     }
 }
